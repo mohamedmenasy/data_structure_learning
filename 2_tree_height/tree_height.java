c161046d8e6e0ac14198b7913180a33a1dd3e6a7
Mohamed Nabil <mohamedmenasy@gmail.com>
2019-07-30 18:34:06 +0200

    fix issue with treeHeight function

--- 2_tree_height/tree_height.java
+++ 2_tree_height/tree_height.java
@@ -1,64 +1,123 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class tree_height {
+    class Node {
+        Queue<Node> childrens;
+
+        Node() {
+            childrens = new LinkedList<>();
+        }
+
+        void addChild(Node node) {
+            childrens.add(node);
+        }
+
+        Node getChild() {
+            return childrens.poll();
+        }
+
+        ArrayList<Node> getChilds() {
+            ArrayList<Node> retNodes = new ArrayList<>();
+            for (Node n : childrens) {
+                retNodes.add(n);
+            }
+            return retNodes;
+        }
+    }
+
     class FastScanner {
-		StringTokenizer tok = new StringTokenizer("");
-		BufferedReader in;
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
 
-		FastScanner() {
-			in = new BufferedReader(new InputStreamReader(System.in));
-		}
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		String next() throws IOException {
-			while (!tok.hasMoreElements())
-				tok = new StringTokenizer(in.readLine());
-			return tok.nextToken();
-		}
-		int nextInt() throws IOException {
-			return Integer.parseInt(next());
-		}
-	}
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
 
-	public class TreeHeight {
-		int n;
-		int parent[];
-		
-		void read() throws IOException {
-			FastScanner in = new FastScanner();
-			n = in.nextInt();
-			parent = new int[n];
-			for (int i = 0; i < n; i++) {
-				parent[i] = in.nextInt();
-			}
-		}
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
 
-		int computeHeight() {
-                        // Replace this code with a faster implementation
-			int maxHeight = 0;
-			for (int vertex = 0; vertex < n; vertex++) {
-				int height = 0;
-				for (int i = vertex; i != -1; i = parent[i])
-					height++;
-				maxHeight = Math.max(maxHeight, height);
-			}
-			return maxHeight;
-		}
-	}
+    public class TreeHeight {
+        int n;
+        int parent[];
+        Node rootNode;
 
-	static public void main(String[] args) throws IOException {
-            new Thread(null, new Runnable() {
-                    public void run() {
-                        try {
-                            new tree_height().run();
-                        } catch (IOException e) {
+        void read() throws IOException {
+            FastScanner in = new FastScanner();
+            n = in.nextInt();
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = in.nextInt();
+            }
+            createTree();
+        }
+
+        void createTree() {
+            List<Node> nodeList = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                nodeList.add(new Node());
+            }
+            for (int i = 0; i < n; i++) {
+                if (parent[i] == -1) {
+                    rootNode = nodeList.get(i);
+                } else {
+                    nodeList.get(parent[i]).addChild(nodeList.get(i));
+                }
+            }
+        }
+
+        int treeHeight(Node node) {
+            if (node == null)
+                return 0;
+            Queue<Node> q = new LinkedList<>();
+            q.add(node);
+            int height = 0;
+            while (true) {
+                int nodeCount = q.size();
+                if (nodeCount == 0)
+                    return height;
+                height++;
+                while (nodeCount > 0) {
+                    Node newNode = q.peek();
+                    q.remove();
+                    if (newNode != null)
+                        if (newNode.childrens != null) {
+                            q.addAll(newNode.getChilds());
                         }
-                    }
-                }, "1", 1 << 26).start();
-	}
-	public void run() throws IOException {
-		TreeHeight tree = new TreeHeight();
-		tree.read();
-		System.out.println(tree.computeHeight());
-	}
+                    nodeCount--;
+                }
+            }
+        }
+
+        int computeHeight() {
+            return treeHeight(rootNode);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Thread(null, new Runnable() {
+            public void run() {
+                try {
+                    new tree_height().run();
+                } catch (IOException e) {
+                }
+            }
+        }, "1", 1 << 26).start();
+    }
+
+    public void run() throws IOException {
+        TreeHeight tree = new TreeHeight();
+        tree.read();
+        System.out.println(tree.computeHeight());
+    }
 }
